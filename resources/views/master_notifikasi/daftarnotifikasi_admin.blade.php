65f2bd4f75c4060ccf2b5885566f748cec3b3d6d
Nolan Tantri <ntantri@gmail.com>
2021-04-23 11:46:59 +0700

    perbaikan minor

--- resources/views/master_notifikasi/daftarnotifikasi_admin.blade.php
+++ resources/views/master_notifikasi/daftarnotifikasi_admin.blade.php
@@ -12,7 +12,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Tambah Data Notifikasi Konsultasi</h1>
+            <h1 class="m-0 text-dark">Daftar Data Notifikasi Konsultasi</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
